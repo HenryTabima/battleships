21a1f9fe0a2e99b00409a1eebae25554dfba4ac2
Sunny <ring2rings93@gmail.com>
2019-07-15 20:32:45 +0100

    board layout with grids

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './styles.scss'
+
+const mainContainer = document.getElementById('main')
+document.addEventListener('DOMContentLoaded', () => {
+  presentBoards()
+  presentBoards()
+})
+
+function presentBoards() {
+  const compGrid = document.createElement('div')
+  compGrid.classList.add('board-container')
+
+  let gridCells = ``
+
+  for (let i = 0; i < 100; i++) {
+    gridCells += `<div class="cell cell-${i}">cell-${i}</div>`
+  }
+  compGrid.innerHTML = gridCells
+
+  mainContainer.appendChild(compGrid)
+
+
+}
